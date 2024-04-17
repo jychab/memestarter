c38115e31d6bc7ae3c09adcd89d8a5c6cf6a4e78
junyangchan <kardxsg@gmail.com>
2024-04-17 21:50:41 +0800

    fix typo

--- app/sections/OnboardScreen.tsx
+++ app/sections/OnboardScreen.tsx
@@ -11,7 +11,7 @@ export const OnboardingScreen: FC = () => {
             <button onClick={() => setHidden(true)}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                width="20x"
+                width="20px"
                 height="20px"
                 viewBox="-0.5 0 25 25"
                 fill="none"
